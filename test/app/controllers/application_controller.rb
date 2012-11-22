571f595b916007a5e0722a0b5cf0c1ccd0cfb8ac
daudi amani <dsaronin@gmail.com>
2012-11-21 21:58:36 -0800

    ctlr test 2 (basic trivial test)

--- test/app/controllers/application_controller.rb
+++ test/app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  attr_accessor :current_user
+  before_filter :set_current_user
+
   rescue_from Kibali::AccessDenied do |e|
     render :text => 'AccessDenied'
+  end
+
+  private
+
+  def set_current_user
+    if params[:user]
+      self.current_user = User.find params[:user]
+    end
   end
 end
--- test/app/controllers/empty_controller.rb
+++ test/app/controllers/empty_controller.rb
@@ -1,17 +1,6 @@
 class EmptyController < ApplicationController
-  attr_accessor :current_user
-  before_filter :set_current_user
-
   [:index, :show, :new, :edit, :update, :delete, :destroy].each do |act|
     define_method(act) { render :text => 'OK' }
-  end
-
-  private
-
-  def set_current_user
-    if params[:user]
-      self.current_user = User.find params[:user]
-    end
   end
 end
 
